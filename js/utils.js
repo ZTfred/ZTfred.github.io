09b3fa5976ccd0242de67603a4804a16ae53c169
zhangtaofred <987828198@qq.com>
2019-08-05 19:38:46 +0800

    Site updated: 2019-08-05 19:38:46

--- js/utils.js
+++ js/utils.js
@@ -1,54 +1,393 @@
-function debounce(func, wait, immediate) {
-  var timeout
-  return function () {
-    var context = this
-    var args = arguments
+/* global Stun, CONFIG */
+
+Stun.utils = Stun.$u = {
+  /**
+   * Debounce
+   * @param {Object} func Callback function
+   * @param {Number} wait Waiting time
+   * @param {Boolean} immediate Run immediately
+   */
+  debounce: function (func, wait, immediate) {
+    var timeout;
+
+    return function () {
+      var context = this;
+      var args = arguments;
+
+      if (timeout) clearTimeout(timeout);
+      if (immediate) {
+        var callNow = !timeout;
+        timeout = setTimeout(function () {
+          timeout = null;
+        }, wait);
+        if (callNow) func.apply(context, args);
+      } else {
+        timeout = setTimeout(function () {
+          func.apply(context, args);
+        }, wait);
+      }
+    };
+  },
+  /**
+   * Throttle
+   * @param {Object} func Callback function
+   * @param {Number} wait Waiting time
+   * @param {Object} options leading: Boolean, trailing: Boolean
+   */
+  throttle: function (func, wait, options) {
+    var timeout, context, args;
+    var previous = 0;
+    if (!options) options = {};
+
     var later = function () {
-      timeout = null
-      if (!immediate) func.apply(context, args)
+      previous = options.leading === false ? 0 : new Date().getTime();
+      timeout = null;
+      func.apply(context, args);
+      if (!timeout) context = args = null;
+    };
+
+    var throttled = function () {
+      var now = new Date().getTime();
+      if (!previous && options.leading === false) previous = now;
+      var remaining = wait - (now - previous);
+      context = this;
+      args = arguments;
+      if (remaining <= 0 || remaining > wait) {
+        if (timeout) {
+          clearTimeout(timeout);
+          timeout = null;
+        }
+        previous = now;
+        func.apply(context, args);
+        if (!timeout) context = args = null;
+      } else if (!timeout && options.trailing !== false) {
+        timeout = setTimeout(later, remaining);
+      }
+    };
+    return throttled;
+  },
+  /**
+   * Change the event code to keyCode.
+   * @param {String} code Event code
+   */
+  codeToKeyCode: function (code) {
+    var codes = {
+      ArrowLeft: 37,
+      ArrowRight: 39,
+      Escape: 27
+    };
+
+    return codes[code];
+  },
+  /**
+   * A UI component for notification prompts.
+   * @param {String} status The Status of message. Values: success / info / warning / error.
+   * @param {String} text The text to show.
+   * @param {Number} delay Message stay time (unit is 's', default 5s).
+   */
+  popAlert: function (status, text, delay) {
+    var icon = {
+      success: 'check-circle',
+      info: 'exclamation-circle',
+      warning: 'exclamation-circle',
+      error: 'times-circle'
+    };
+
+    if (!$('.stun-alert')[0]) {
+      var $alert = $(
+        '<div class="stun-message">' +
+          '<div class="stun-alert stun-alert-' + status + '">' +
+            '<i class="stun-alert-icon fa fa-' + icon[status] + '"></i>' +
+            '<span class="stun-alert-description">' + text + '</span>' +
+          '</div>' +
+        '</div>'
+      );
+
+      $('body').append($alert);
     }
-    var callNow = immediate && !timeout
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
-    if (callNow) func.apply(context, args)
+
+    $(document).ready(function () {
+      $('.stun-alert')
+        .velocity('stop')
+        .velocity('transition.slideDownBigIn', {
+          duration: 300
+        })
+        .velocity('reverse', {
+          delay: delay * 1000 || 5000,
+          duration: 260,
+          complete: function () {
+            $('.stun-alert').css('display', 'none');
+          }
+        });
+    });
+  },
+  /**
+   * Copy any text.
+   * @param {HTMLElement} container Container of text.
+   */
+  copyText: function (container) {
+    try {
+      var selection = window.getSelection();
+      var range = document.createRange();
+
+      // Select text by the content of node.
+      range.selectNodeContents(container);
+      selection.removeAllRanges();
+      selection.addRange(range);
+
+      var text = selection.toString();
+      var input = document.createElement('input');
+
+      // Create a temporary input to make the
+      // execCommand command take effect.
+      input.style.display = 'none';
+      input.setAttribute('readonly', 'readonly');
+      input.setAttribute('value', text);
+      document.body.appendChild(input);
+      input.setSelectionRange(0, -1);
+
+      if (document.execCommand('copy')) {
+        document.execCommand('copy');
+        document.body.removeChild(input);
+
+        return true;
+      }
+      document.body.removeChild(input);
+    } catch (e) {
+      return false;
+    }
+  },
+  // Wrap images with fancybox support.
+  wrapImageWithFancyBox: function () {
+    $('.content img').not(':hidden').each(function () {
+      var $img = $(this);
+      var imgTitle = $img.attr('title') || $img.attr('alt');
+      var $imgWrap = $img.parent('a');
+
+      if (!$imgWrap[0]) {
+        var imgSrc = $img.attr('data-original') || $img.attr('src');
+
+        $imgWrap = $img.wrap('<a class="fancybox" href="' + imgSrc +
+          '" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>'
+        ).parent('a');
+
+        if ($img.is('.gallery img')) {
+          $imgWrap.attr('data-fancybox', 'gallery');
+        } else {
+          $imgWrap.attr('data-fancybox', 'default');
+        }
+      }
+
+      if (imgTitle) {
+        $imgWrap.attr('title', imgTitle).attr('data-caption', imgTitle);
+      }
+    });
+
+    $().fancybox({
+      selector: '[data-fancybox]',
+      loop: true,
+      transitionEffect: 'slide',
+      buttons: [
+        'share',
+        'slideShow',
+        'fullScreen',
+        'download',
+        'thumbs',
+        'close'
+      ]
+    });
+  },
+  // Display the image in the gallery as a waterfall.
+  galleryWaterFall: function () {
+    var gConfig = CONFIG.gallery_waterfall;
+    var colWidth = parseInt(gConfig.col_width);
+    var colGapX = parseInt(gConfig.gap_x);
+
+    this.waitAllImageLoad('.gallery img', function () {
+      $('.gallery').masonry({
+        itemSelector: '.gallery-image',
+        columnWidth: colWidth,
+        percentPosition: true,
+        gutter: colGapX,
+        transitionDuration: 0
+      });
+    });
+  },
+  // Lazy load the images of post.
+  lazyLoadImages: function () {
+    $('img.lazyload').lazyload();
+  },
+  // Add a mark icon to the link with `target="_blank"` attribute.
+  addIconToExternalLink: function (container) {
+    if (!$(container)[0]) return;
+
+    var $wrapper = $('<span class="external-link"></span>');
+    var $icon = $(
+      '<i class="fa fa-' +
+        CONFIG.external_link.icon.name +
+      '"></i>'
+    );
+
+    $(container)
+      .find('a[target="_blank"]')
+      .wrap($wrapper)
+      .parent('.external-link')
+      .append($icon);
+  },
+  // Back the page to top.
+  back2top: function () {
+    function back2topHandler () {
+      var $top = $('#back-top');
+      var scrollTop = $(window).scrollTop();
+
+      if (scrollTop !== 0) {
+        $top.css('visibility', 'visible');
+      } else {
+        $top.css('visibility', 'hidden');
+      }
+    }
+
+    $(window).on('load', back2topHandler);
+    $(window).on('scroll', back2topHandler);
+
+    $('#back-top').on('click', function () {
+      $('body').velocity('stop').velocity('scroll');
+
+      if (CONFIG.back2top.animation) {
+        $('#back-top')
+          .velocity({ translateY: '-100vh' }, { duration: 500 })
+          .velocity('reverse', { duration: 10 });
+      }
+    });
+  },
+  // Switch to the prev / next post by shortcuts.
+  registerSwitchPost: function () {
+    var _this = this;
+
+    $(document).on('keydown', function (ev) {
+      var e = ev || window.event;
+      var isPrev = e.keyCode === _this.codeToKeyCode('ArrowLeft');
+      var isNext = e.keyCode === _this.codeToKeyCode('ArrowRight');
+
+      if (e.ctrlKey && isPrev) {
+        var prev = $('.article-prev').find('a')[0];
+        prev && prev.click();
+      } else if (e.ctrlKey && isNext) {
+        var next = $('.article-next').find('a')[0];
+        next && next.click();
+      }
+    });
+  },
+  // Show / Hide the reward QR.
+  registerShowReward: function () {
+    $('.reward-button').on('click', function () {
+      var $container = $('.reward-qr-wrapper');
+
+      if ($container.is(':visible')) {
+        $container.css('display', 'none');
+      } else {
+        $container
+          .velocity('stop')
+          .velocity('transition.slideDownBigIn', {
+            duration: 300
+          });
+      }
+    });
+  },
+  // Click to zoom in image, without fancybox.
+  registerClickToZoomImage: function () {
+    $('.content img').not(':hidden').each(function () {
+      $(this).addClass('zoom-image');
+    });
+
+    var $newImgMask = $('<div class="zoom-image-mask"></div>');
+    var $newImg = $('<img>');
+    var isZoom = false;
+
+    $(window).on('scroll', function () {
+      if (isZoom) {
+        isZoom = false;
+        setTimeout(closeZoom, 200);
+      }
+    });
+
+    $(document).on('click', function () {
+      closeZoom();
+    });
+
+    $('.zoom-image').on('click', function (ev) {
+      var e = ev || window.event;
+      e.stopPropagation();
+      isZoom = true;
+
+      var imgRect = this.getBoundingClientRect();
+      var imgW = $(this).width();
+      var imgH = $(this).height();
+      var imgOuterW = $(this).outerWidth();
+      var imgOuterH = $(this).outerHeight();
+      var winW = $(window).width();
+      var winH = $(window).height();
+      var scaleX = winW / imgW;
+      var scaleY = winH / imgH;
+      var scale = (scaleX < scaleY ? scaleX : scaleY) || 1;
+      var translateX = winW / 2 - (imgRect.x + imgOuterW / 2);
+      var translateY = winH / 2 - (imgRect.y + imgOuterH / 2);
+
+      $newImg.attr('class', this.className);
+      $newImg.attr('src', this.src);
+      $newImg.addClass('show');
+      $newImg.css({
+        left: $(this).offset().left + (imgOuterW - imgW) / 2,
+        top: $(this).offset().top + (imgOuterH - imgH) / 2,
+        width: imgW,
+        height: imgH
+      });
+
+      $(this).addClass('hide');
+      $('body').append($newImgMask).append($newImg);
+      $newImgMask.velocity({ opacity: 1 });
+      $newImg.velocity({
+        translateX: translateX,
+        translateY: translateY,
+        scale: scale
+      }, {
+        duration: 300,
+        easing: [0.2, 0, 0.2, 1]
+      });
+    });
+
+    function closeZoom () {
+      $newImg.velocity('reverse');
+      $newImgMask.velocity('reverse', {
+        complete: function () {
+          $('.zoom-image.show').remove();
+          $('.zoom-image-mask').remove();
+          $('.zoom-image').removeClass('hide');
+        }
+      });
+    }
+  },
+  /**
+   * Wait for all images to load.
+   * @param {String} selector jQuery selector.
+   * @param {Function} callback Callback.
+   */
+  waitAllImageLoad: function (selector, callback) {
+    var imgDefereds = [];
+
+    $(selector).each(function () {
+      var dfd = $.Deferred();
+      $(this).bind('load', function () {
+        dfd.resolve();
+      });
+
+      if (this.complete) {
+        setTimeout(function () {
+          dfd.resolve();
+        }, 500);
+      }
+      imgDefereds.push(dfd);
+    });
+    $.when.apply(null, imgDefereds).then(callback);
   }
 };
-
-function throttle(func, wait, mustRun) {
-  var timeout
-  var startTime = new Date()
-
-  return function () {
-    var context = this
-    var args = arguments
-    var curTime = new Date()
-
-    clearTimeout(timeout)
-    if (curTime - startTime >= mustRun) {
-      func.apply(context, args)
-      startTime = curTime
-    } else {
-      timeout = setTimeout(func, wait)
-    }
-  }
-};
-
-function isMobile() {
-  var check = false;
-  (function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera)
-  return check
-};
-
-function scrollTo(name) {
-  var scroll_offset = $(name).offset();
-  $("body,html").animate({
-    scrollTop: scroll_offset.top
-  })  
-};
-
-window.debounce = debounce
-
-window.throttle = throttle
-
-window.isMobile = isMobile
-
